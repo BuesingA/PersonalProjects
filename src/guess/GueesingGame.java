1b65376a3d65d80bb7b2e3896fceb2d061db2da5
BuesingA <31742835+BuesingA@users.noreply.github.com>
2018-03-03 00:16:56 -0600

    Add files via upload

--- src/guess/GueesingGame.java
+++ src/guess/GueesingGame.java
@@ -1,5 +1,7 @@
 package guess;
 
+import static java.lang.System.in;
+
 import java.util.Scanner;
 
 public class GueesingGame {
@@ -9,6 +11,9 @@ public class GueesingGame {
 		int key = (int) Math.ceil(Math.random() * 100);
 		boolean right = false;
 		int count = 1;
+		int overall = 0;
+		int times = 0;
+		double avg =0;
 		System.out.println("Welcome to the guessing game! Try to guess the number in as few tries as possible!");
 		
 		while (!right) {
@@ -18,6 +23,20 @@ public class GueesingGame {
 			if(key==gs) {
 				System.out.println("Congrats you got it right in " + count + " guesses");
 				right=true;
+				overall+=count;
+				times++;
+				System.out.println("Would you like to play again? y or n");
+				Scanner keyboard = new Scanner(System.in);
+				String ans = keyboard.next();
+				if(ans.equals("y")) {
+					right=false;
+					count=1;
+					key=(int) Math.ceil(Math.random() * 100);
+				}
+				if(ans.equals("n")) {
+					avg = (overall/times);
+					System.out.println("You completed the game " + times + " times, with an average number of guesses of " + avg);
+				}
 			}
 			else if (key>gs) { 
 				System.out.println("The answer is greater than " + gs);
